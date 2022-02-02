5e3f5d197bb9221f216aa9e3a07598fcbeab65b6
Jiyoung Wi <jywi@akcco.com>
2022-02-02 20:56:47 +0900

    Binding01 : ComboBox를 Dictionary와 BindingSource로 묶기 - 테스트완료 푸쉬

---
+++ Binding01/Binding01/formMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Binding01
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValue = new System.Windows.Forms.Button();
+            this.btnText = new System.Windows.Forms.Button();
+            this.cbTest = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnValue
+            // 
+            this.btnValue.Location = new System.Drawing.Point(274, 27);
+            this.btnValue.Name = "btnValue";
+            this.btnValue.Size = new System.Drawing.Size(150, 55);
+            this.btnValue.TabIndex = 0;
+            this.btnValue.Text = "Show the Value";
+            this.btnValue.UseVisualStyleBackColor = true;
+            this.btnValue.Click += new System.EventHandler(this.btnValue_Click);
+            // 
+            // btnText
+            // 
+            this.btnText.Location = new System.Drawing.Point(274, 88);
+            this.btnText.Name = "btnText";
+            this.btnText.Size = new System.Drawing.Size(150, 55);
+            this.btnText.TabIndex = 1;
+            this.btnText.Text = "Show the Text";
+            this.btnText.UseVisualStyleBackColor = true;
+            this.btnText.Click += new System.EventHandler(this.btnText_Click);
+            // 
+            // cbTest
+            // 
+            this.cbTest.FormattingEnabled = true;
+            this.cbTest.Location = new System.Drawing.Point(36, 77);
+            this.cbTest.Name = "cbTest";
+            this.cbTest.Size = new System.Drawing.Size(204, 23);
+            this.cbTest.TabIndex = 2;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 178);
+            this.Controls.Add(this.cbTest);
+            this.Controls.Add(this.btnText);
+            this.Controls.Add(this.btnValue);
+            this.Name = "formMain";
+            this.Text = "TEST";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValue;
+        private System.Windows.Forms.Button btnText;
+        private System.Windows.Forms.ComboBox cbTest;
+    }
+}
+
---
+++ Binding01/Binding01/formMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// ComboBox data들을 Dictionary를 사용하여  bindingSource로 데이터 묶기
+/// </summary>
+namespace Binding01
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dictionary<string, string> keyValues = new Dictionary<string,string>();
+            keyValues.Add("1", "First");
+            keyValues.Add("2", "Second");
+
+            cbTest.DataSource = new BindingSource(keyValues, null);
+            cbTest.ValueMember = "Key";
+            cbTest.DisplayMember = "Value";
+        }
+
+        private void btnText_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cbTest.Text);
+            //MessageBox.Show(cbTest.SelectedText);
+            /*콤보 상자에서 현재 선택되어 있는 텍스트를 나타냄
+             * DropDownStyle을 DropDownList으로 설정하면 빈 문자열("")이 반환됩니다.
+             * 고로 현 시점에서는 적용안되는 함수*/
+        }
+
+        private void btnValue_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cbTest.SelectedValue.ToString());
+        }
+    }
+}
